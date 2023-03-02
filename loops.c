b3751f55a6da61a4680a2950e03d582e99834898
Nasika Balaji <balajinani94@gmail.com>
2023-03-02 12:16:07 +0530

    loops

---
+++ loops.c
@@ -0,0 +1,77 @@
+#if condition:
+
+#checking positive number
+'''a=int(input('enter a number: '))
+if a>=0:
+    print('positive number')'''   
+
+
+
+
+#Nested if conditions
+
+ '''a=int(input('enter a number: '))
+b=int(input('enter a number: '))
+c=int(input('enter a number: '))
+if a>b:
+    if a>c:
+        print('a is big number')
+    else:
+        print('c is big number')
+else:
+    if b>c:
+        print('b is big number')
+    else:
+        print('c is big number')'''
+
+
+
+
+#if elif else conditions
+    
+#positive or negative numbers
+'''a=int(input("enter a number: "))
+if a>0:
+    print('It is a positive number:',a)
+elif a==0:
+    print('It is zero')
+else:
+    print('It is negative number:',a)'''
+
+
+#greater or smaller number
+'''a=int(input('enter a number: '))
+b=int(input('enter a number: '))
+if a>b:
+    print('a is greater than b')
+elif a<b:
+    print('a is smaller than b')
+else:
+    print('both are equal')'''
+
+
+
+
+#if else conditions
+    
+#even or odd
+'''num=int(input("enter a number: "))
+if num%2==0:
+    print("It is even number:", num)
+else:
+    print("It is odd number: ", num)'''
+
+'''n=int(input('enter a number: '))
+if n%2==0:
+    print('{} is even'.format(n))
+else:
+    print('{} is odd'.format(n))  '''
+
+
+#largest number
+'''n1=int(input('enter a number: '))
+n2=int(input('enter a number: '))
+if n1>n2:
+    print('n1 is greater',n1)
+else:
+    print('n2 is greater',n2)'''
