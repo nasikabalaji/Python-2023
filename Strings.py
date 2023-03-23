72004272385de63c98554b19d915398351d63746
nasikabalaji <126751769+nasikabalaji@users.noreply.github.com>
2023-03-23 10:41:58 +0530

    String Slicing

---
+++ Strings.py
@@ -0,0 +1,16 @@
+string="python"
+print(string[:])
+# printing all the characters in string
+
+print(string[1:])
+# printing the characters from index 1
+
+print(string[:4])
+# printing the first 4 characters from the string
+
+print(string[0:5:2])
+# printing alternate characters from the string
+
+print(string[-4:-1])
+
+print(string[-4:-1:2])
