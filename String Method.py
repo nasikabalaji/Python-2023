df154afec2ff077061da286e96261a1518e46426
nasikabalaji <126751769+nasikabalaji@users.noreply.github.com>
2023-03-24 10:04:08 +0530

    String Methods

---
+++ String Method.py
@@ -0,0 +1,71 @@
+'''a='   Hello   '
+print(a.strip())'''
+
+'''a="   python   "
+print(a.lstrip())'''
+
+'''a="   python   "
+print(a.rstrip())'''
+
+'''b='Welcome to my world!'
+print(b.replace('o','k'))'''
+
+'''e='welcome to python'
+print(e.endswith('on'))'''
+
+'''f='welcome to python'
+print(f.startswith('wel'))'''
+
+'''g='welcome to python programming'
+print(g.title())'''
+
+'''a='welcome to python programming'
+print(a.istitle())'''
+
+'''h='python'
+print(h.center(10,'*'))'''
+
+'''i='WelCOme'
+print(i.swapcase())'''
+
+'''j='python'
+print(j.upper())'''
+
+'''b='python'
+print(b.isupper())'''
+
+'''k='PYTHON'
+print(k.lower())'''
+
+'''l='python'
+print(l.islower())'''
+
+'''a="welcome to python programming"
+print(a.find('o'))'''
+
+'''b="welcome to python programming"
+print(b.rfind('o'))'''
+
+'''c="welcome to python programming"
+print(c.index('m'))'''
+
+'''d="welcome to python programming"
+print(d.rindex('o'))'''
+
+'''e="welcome to python programming"
+print(e.isalpha())'''
+
+'''f="3456"
+print(f.isnumeric())'''
+
+'''g=' '
+print(g.isspace())'''
+
+'''a='python'
+print(a.ljust(10,"-"))'''
+
+'''b='python'
+print(b.rjust(10,"-"))'''
+
+'''c='python'
+print(c.zfill(10))'''
